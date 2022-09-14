92c423246433240ce752633e832e88687d536447
calebefullsucess <caleberamalhomir4@gmail.com>
2022-09-14 08:04:05 -0300

    v2

---
+++ let.js
@@ -0,0 +1,16 @@
+
+let nome = 'angelo';
+let filho = 'julho'
+let esposa = 'ana'
+let data = '1990'
+let idade; 
+idade = '31'
+
+console.log( nome,  'nasceu em 1887');
+console.log( nome,'tem',idade);
+console.log('em', data, nome, 'coheceu',esposa);
+console.log(nome, 'casou se com', esposa);
+console.log(sesposa, 'teve 1 filho com', nome, 'em 2015');
+console.log('o filho de', nome, 'se chama',filho);
+
+
